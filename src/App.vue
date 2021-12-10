66a9bb9b9aeaea95174ab04ec031813fb0e21dd4
uphoon <uphoon@gmail.com>
2021-12-10 02:28:43 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- img alt="Vue logo" src="./assets/logo.png" / -->
+  <Plus />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Plus from './components/Plus.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
